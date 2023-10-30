9a03560574b5de5f6c72f1921a568a1968251e61
Oğuzhan Duman <76626056+roco999999@users.noreply.github.com>
2023-10-31 00:31:15 +0300

    Delete atm-demo directory

--- atm-demo/src/main/java/com/atm/atmdemo/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atm.atmdemo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.atm.atmdemo.model.Transaction;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
