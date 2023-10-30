f90ebef015b4d4a43ba14496b3c64e649c3b7a96
Oğuzhan Duman <76626056+roco999999@users.noreply.github.com>
2023-10-30 23:05:18 +0300

    Add files via upload

---
+++ atm-demo/src/main/java/com/atm/atmdemo/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.atm.atmdemo.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
+
---
+++ atm-demo/src/main/java/com/atm/atmdemo/service/TransactionService.java
@@ -0,0 +1,47 @@
+package com.atm.atmdemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.atm.atmdemo.model.Account;
+import com.atm.atmdemo.model.Transaction;
+import com.atm.atmdemo.repository.AccountRepository;
+import com.atm.atmdemo.repository.TransactionRepository;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
+    private final AccountService accountService;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository, AccountRepository accountRepository, AccountService accountService) {
+        this.transactionRepository = transactionRepository;
+        this.accountRepository = accountRepository;
+        this.accountService = accountService;
+    }
+
+    public Transaction processTransaction(int accountId, double amount, String type) {
+    	
+        Account account = null;
+        
+        // İşlem türüne göre işlemi gerçekleştir
+        if ("DEPOSIT".equals(type)) {
+            account = accountService.deposit(accountId, amount);
+        } else if ("WITHDRAW".equals(type)) {
+            account = accountService.withdraw(accountId, amount);
+        } else {
+            throw new RuntimeException("Geçersiz işlem türü");
+        }
+        
+        // Yeni işlem oluştur
+        Transaction transaction = new Transaction();
+        transaction.setAccount(account);
+        transaction.setAmount(amount);
+        transaction.setType(type);
+        
+        // İşlemi kaydet
+        return transactionRepository.save(transaction);
+    }
+}
+
